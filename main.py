6835e1e9f72360389a59c291c258f693bf4c59e5
Jamie Folsom <jamiefolsom@gmail.com>
2016-01-19 11:16:45 -0500

    Make sentinels a list, and add :metal: to default sentinels.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def valid_date(s):
         raise argparse.ArgumentTypeError(msg)
 
 
-def main(sentinel, repos, token, since):
+def main(sentinels, repos, token, since):
     """
     Loop through merged prs in the available repos to find comments which
     contain sentinel values. Credit those users with reviewing it.
@@ -33,15 +33,16 @@ def main(sentinel, repos, token, since):
     total_reviews = []
     for pr in to_check:
         for comment in pr.get_issue_comments():
-            if sentinel in comment.body:
-                total_reviews.append(comment.user.name)
+            for sentinel in sentinels:
+                if sentinel in comment.body:
+                    total_reviews.append(comment.user.name)
 
     counter = collections.Counter(total_reviews)
     print '\n'.join(['%s: %s' % (x, y) for x,y in counter.most_common()])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Count reviews on github")
-    parser.add_argument('--sentinel', dest='sentinel', default=':+1:', help='String to look for to count something as "reviewed"')
+    parser.add_argument('--sentinels', dest='sentinels', default='[":+1:", ":metal:"]', help='One or more strings to look for to count something as "reviewed"')
     parser.add_argument('--repo', dest='repo', action='append', help="Repos to search through")
     parser.add_argument('--token', dest='token', help="Your github personal access token")
     parser.add_argument('--since', dest='since', type=valid_date, help="YYYY-MM-DD format of when to outer bound the request")
@@ -53,4 +54,4 @@ if __name__ == '__main__':
     if not args.since:
         raise RuntimeError("You must specify a valid time to start the check")
 
-    main(args.sentinel, args.repo, args.token, args.since)
+    main(args.sentinels, args.repo, args.token, args.since)
